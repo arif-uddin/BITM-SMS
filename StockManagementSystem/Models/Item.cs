544273b52e26f1d07eb06fcbb896120bd126d505
akash4gb <akash4gb@gmail.com>
2018-07-31 01:02:51 +0600

    Added Models/classes

---
+++ StockManagementSystem/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem.Models
+{
+    class Item
+    {
+        public int Category { get; set; }
+        public int Company { get; set; }
+        public string Name { get; set; }
+        public int ReorderLevel { get; set; }   
+        
+    }
+}
---
+++ StockManagementSystem/Models/StockIn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem.Models
+{
+    class StockIn
+    {
+        public int Company { get; set; }
+        public int Category { get; set; }
+        public int Qantity { get; set; }          
+    }
+}
